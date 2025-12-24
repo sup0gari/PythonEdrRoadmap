ec4d99d71b10e642b9717be56ccb2cadeb8d714d
sup0gari <kuri.ladybaby1012@gmail.com>
2025-12-24 12:34:37 +0900

    windows event log detection created

--- edr.py
+++ edr.py
@@ -2,6 +2,7 @@ import os
 import time
 import hashlib
 import psutil
+import win32evtlog
 
 TARGET_FILE = "test.txt"
 
@@ -31,6 +32,25 @@ def get_modifying_process(file):
             continue
     return None
 
+def get_event_log(file):
+    server = 'localhost'
+    log_type = 'Security'
+    handle = win32evtlog.OpenEventLog(server, log_type)
+
+    # Windowsイベントログを最新から読み取る
+    flags = win32evtlog.EVENTLOG_BACKWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ
+    events = win32evtlog.ReadEventLog(handle, flags, 0)
+
+    for event in events:
+        # イベントID 4663（ファイル操作）で対象ファイルかどうか
+        if event.EventID == 4663 and file in str(event.StringInserts):
+            return {
+                "process_name": event.StringInserts[6], # 実行ファイル名
+                "user": event.StringInserts[1], # 実行ユーザー名
+                "pid": event.StringInserts[7], # PID
+            }
+    return None
+
 last_mtime = get_mtime(TARGET_FILE)
 last_hash = calc_hash(TARGET_FILE)
 print(f"[*] Monitoring started for {TARGET_FILE}")
@@ -41,13 +61,23 @@ while True:
     current_hash = calc_hash(TARGET_FILE)
     if current_mtime != last_mtime:
         if current_hash != last_hash:
-            proc_info = get_modifying_process(TARGET_FILE)
             print(f"[!!] CRITICAL: File content modified!")
+            proc_info = get_modifying_process(TARGET_FILE) # psutilのリアルタイムスキャン
+            
+            # psutil失敗用
+            if not proc_info:
+                print("    [*] Real-time scan failed. Cheking Windows Event Logs...")
+                proc_info = get_event_log(TARGET_FILE)
+            
             if proc_info:
-                print(f"    Process: {proc_info['name']} (PID: {proc_info['pid']})")
-                print(f"    User: {proc_info['username']}")
+                process_name = proc_info.get('name') or proc_info.get('process_name')
+                pid = proc_info.get('pid') or proc_info.get('pid')
+                user = proc_info.get('username') or proc_info.get('user')
+
+                print(f"    Process: {process_name} (PID: {pid})")
+                print(f"    User: {user}")
             else:
-                print("    Process: Could not be identified (Closed too fast)")
+                print("    [?] Evidence not found in Event Logs yet. (Wait for OS to write log)")
             print(f"    New Hash: {current_hash}")
             
             last_hash = current_hash
